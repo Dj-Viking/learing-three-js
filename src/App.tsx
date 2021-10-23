f6d0665b7b18d599fa0b94f8f7172b8030f35369
dj-viking <anders.swedishviking@gmail.com>
2021-10-23 18:17:36 -0400

    playing with cube rotation stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import './App.css';
 import * as THREE from "three";
 
 const App: React.FC<Record<string, unknown>> = () => {
-  const { innerHeight } = window;
-  const { innerWidth } = window;
+  const { innerHeight, innerWidth } = window;
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
   const renderer = new THREE.WebGLRenderer({
@@ -13,12 +12,8 @@ const App: React.FC<Record<string, unknown>> = () => {
   renderer.setSize(innerWidth, innerHeight);
 
   window.document.body.appendChild(renderer.domElement);
-  window.addEventListener("mousemove", (event) => {
-    console.log("mouse event CLIENT X Y", event.clientX, event.clientY);
-    camera.position.z = 5 + event.clientY / 30;
-  });
 
-  const geometry = new THREE.BoxGeometry();
+  const geometry = new THREE.BoxGeometry(5, 5, 5, 10, 10, 0);
   const material = new THREE.MeshBasicMaterial({   
     color: 0x00ff00,
     wireframe: true
@@ -26,12 +21,16 @@ const App: React.FC<Record<string, unknown>> = () => {
 
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
+  window.document.addEventListener("mousemove", (event) => {
+    // console.log("mouse event CLIENT X Y", event.clientX, event.clientY);
+    camera.position.z = 5 + (event.clientY / 30) / 2;
+  });
 
   function animate(): void {
-    requestAnimationFrame(animate);
+    const num = requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.05;
+    cube.rotation.x += num / 10000 + 0.01;
+    cube.rotation.y += num / 10000;
     renderer.render(scene, camera);
   }
 
